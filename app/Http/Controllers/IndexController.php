80552ba2cb5e433866ae28c081f5a86685acbf88
zrn <770706556@qq.com>
2019-11-28 17:31:26 +0800

    111

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,10 +8,22 @@ class IndexController extends Controller{
     public function index(){
        // DB::setFetchMode(PDO::FETCH_ASSOC);
         $user=DB::table('user')->get();
-        foreach ($user as $val) {
+        /*foreach ($user as $val) { //打印name列
             echo $val->name;
-        }
+        }*/
        // $user = DB::select('select * from user', [1]);
-    //    print_r(json_decode(json_encode($user),true));
+        print_r(json_decode(json_encode($user),true));
     }
+
+    public function update(){
+
+    }
+
+    public function add(){
+        DB::table('user')->insert(
+            ['name' => '测试123']
+        );
+        return 'success';
+    }
+
 }
