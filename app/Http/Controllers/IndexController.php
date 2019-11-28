364a78c7c7a80fea23a6e54f287a5e1f1f370dd7
zrn <770706556@qq.com>
2019-11-28 16:02:31 +0800

    12312

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,7 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 class IndexController extends Controller{
     public function index(){
-        $user=DB::name('user')->get();
+      //  $user=DB::name('user')->get();
+        $user = DB::select('select * from user', [1]);
         print_r($user);
     }
 }
