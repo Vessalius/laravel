63af950b452f2a45303c6184ffafc8e7f38a31cc
zrn <770706556@qq.com>
2019-11-28 15:27:43 +0800

    添加index控制器

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+    public function index(){
+        echo 11111;
+    }
+}
