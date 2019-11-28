da959f10ce6ba3f865aaaea9212a50b20f75f718
zrn <770706556@qq.com>
2019-11-28 16:31:29 +0800

    111

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use PDO;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 class IndexController extends Controller{
     public function index(){
+        DB::setFetchMode(PDO::FETCH_ASSOC);
         $user=DB::table('user')->get();
        // $user = DB::select('select * from user', [1]);
         print_r($user);
